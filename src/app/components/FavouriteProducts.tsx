53e9270dc3d5fd275b12688810bcae241214fecc
Loai Rebouh <loai_rebouh@icloud.com>
2025-01-03 22:38:28 +0100

    refacto(favourite products): add a beautiful newst product list

--- src/app/components/FavouriteProducts.tsx
+++ src/app/components/FavouriteProducts.tsx
@@ -4,30 +4,27 @@ import customTitle from "./ui/customTitle";
 import { fetchProducts } from "../../../lib/fetchProducts";
 import { urlFor } from "../../../lib/sanity";
 import { Product } from "../../../types";
-
+import { qatarBold, qatarMeduim } from "./Fonts";
 interface FavouriteProductsProps {
   products: Product[];
 }
-
 const FavouriteProducts: React.FC<FavouriteProductsProps> = ({ products }) => {
   const firstFourProducts = products?.slice(0, 4) || [];
-
-  // Helper function to determine the price
   const getPrice = (product: Product) => {
     if (product.hasVolume) {
       const firstVolume = product.volumesAndPricesAndDiscounts?.[0];
       if (firstVolume) {
         if (firstVolume.pricing.hasDiscount) {
-          return `${firstVolume.pricing.discount?.newPrice || "N/A"} DZD`;
+          return `${firstVolume.pricing.discount?.newPrice || "N/A"} `;
         }
-        return `${firstVolume.pricing.dzd || "N/A"} DZD`;
+        return `${firstVolume.pricing.dzd || "N/A"} `;
       }
     } else {
       const { basicPriceAndDiscount } = product;
       if (basicPriceAndDiscount?.hasDiscount) {
-        return `${basicPriceAndDiscount.discount?.newPrice || "N/A"} DZD`;
+        return `${basicPriceAndDiscount.discount?.newPrice || "N/A"} `;
       }
-      return `${basicPriceAndDiscount?.dzd || "N/A"} DZD`;
+      return `${basicPriceAndDiscount?.dzd || "N/A"} `;
     }
     return "Price not available";
   };
@@ -40,38 +37,38 @@ const FavouriteProducts: React.FC<FavouriteProductsProps> = ({ products }) => {
           const imageUrl = product.productPicture?.asset?._ref
             ? urlFor(product.productPicture).width(400).url()
             : "/default-image.jpg";
-
-          // Get price for the product
           const priceDisplay = getPrice(product);
-
           return (
-            <div
-              key={product._id || Math.random()}
-              className="rounded-lg bg-white shadow-md"
-            >
+            <div key={product._id || Math.random()} className="rounded-lg">
               <div>
                 <img
                   src={imageUrl}
                   alt={product.productName?.en || "Product Image"}
-                  className="h-52 w-full rounded-t-lg object-cover object-center"
+                  className="mx-auto h-full w-full rounded-lg object-cover"
                 />
               </div>
-              <div className="p-4">
-                <h1 className="text-lg font-semibold text-gray-900">
-                  {product.productName?.en || "Unnamed Product"}
-                </h1>
-                <p className="mt-2 text-sm text-gray-500">
-                  {product.description?.en || "No description available."}
-                </p>
-                <div className="mt-4 flex items-center justify-between">
-                  <p className="text-lg font-semibold text-primary">
-                    {priceDisplay !== "N/A DZD"
-                      ? priceDisplay
-                      : "Price not available"}
-                  </p>
-                  <button className="rounded-md bg-primary px-4 py-2 text-white">
-                    View Product
-                  </button>
+              <div>
+                <div className="mt-5">
+                  <div>
+                    <div
+                      className={`text-2xl font-semibold text-gray-900 ${qatarBold.className}`}
+                    >
+                      {product.productName?.en || "Unnamed Product"} <br />
+                      {product.productName?.ar || "u"}
+                    </div>
+                    <div className="mt-2 flex flex-row items-start justify-end gap-2">
+                      <div
+                        className={`font-semibold text-primary ${qatarBold.className} text-3xl`}
+                      >
+                        {priceDisplay !== "N/A DZD"
+                          ? priceDisplay
+                          : "Price not available"}
+                      </div>
+                      <div className={`${qatarMeduim.className} text-primary`}>
+                        DZD
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -81,13 +78,9 @@ const FavouriteProducts: React.FC<FavouriteProductsProps> = ({ products }) => {
     </div>
   );
 };
-
-// Fetch data in getServerSideProps
 export async function getServerSideProps() {
   try {
-    // Fetch product data
     const products = await fetchProducts();
-
     return {
       props: {
         products,
